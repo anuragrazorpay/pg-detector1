b8be33cf6c8a17bbd7edcd05875c824dd8764e74
anuragrazorpay <anurag.rastogi@razorpay.com>
2025-05-20 14:41:29 +0530

    Create userAgents.js

---
+++ userAgents.js
@@ -0,0 +1,7 @@
+export const USER_AGENTS = [
+  // A set of real-world UAs for Chrome, Firefox, Safari, Edge, Android, iPhone
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15',
+  'Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36',
+  'Mozilla/5.0 (iPhone; CPU iPhone OS 16_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Mobile/15E148 Safari/604.1'
+];
