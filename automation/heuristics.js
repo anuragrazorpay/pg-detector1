450156b1613fc10b31e56c53e74e8482d1b5d09d
anuragrazorpay <anurag.rastogi@razorpay.com>
2025-05-20 14:41:53 +0530

    Create heuristics.js

---
+++ automation/heuristics.js
@@ -0,0 +1,13 @@
+export function findLikelyButtons(elements, keywords = ['add to cart', 'buy', 'checkout', 'cart', 'pay']) {
+  const candidates = [];
+  for (const el of elements) {
+    const text = (el.innerText || el.ariaLabel || '').toLowerCase();
+    for (const kw of keywords) {
+      if (text.includes(kw)) {
+        candidates.push(el.selector);
+        break;
+      }
+    }
+  }
+  return candidates;
+}
