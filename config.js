234e8cc9021eb7892b95f382f845edce6a96264b
anuragrazorpay <anurag.rastogi@razorpay.com>
2025-05-20 19:34:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const config = {
   timeout: 35000,
   maxRetries: 5,
-  headless: true, // set true for stealth mode
+  headless: false, // set true for stealth mode
   slowMo: 50, // ms between actions
   evidenceDir: './evidence',
   geminiModel: 'gemini-1.5-pro-latest', // update as needed
