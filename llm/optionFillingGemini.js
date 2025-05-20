762409cdfaa485ac01c37980cb6ca6d743717389
anuragrazorpay <anurag.rastogi@razorpay.com>
2025-05-20 20:23:24 +0530

    Update optionFillingGemini.js

--- llm/optionFillingGemini.js
+++ llm/optionFillingGemini.js
@@ -4,7 +4,7 @@ dotenv.config();
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { config } from '../config.js';
 
-const model = genAI.getGenerativeModel({ model: config.geminiModel });
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 /**
  * @param {Array} optionsArr - Array of {tagName, type, innerText, ariaLabel, id, class, selector}
